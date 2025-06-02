d68882de9c3b45c0210cd51dd71e7bc867017f72
Jlarrinzal <jose.maria.larrinzal@estudiantat.upc.edu>
2025-06-02 12:43:27 +0200

    fix: jwt validator without Http, Secure, SameSite

--- kong-plugins/jwt_validator/handler.lua
+++ kong-plugins/jwt_validator/handler.lua
@@ -30,7 +30,7 @@ function plugin:access(conf)
   end
   
   if jwt_obj.verified then
-  kong.response.set_header("Set-Cookie", "auth_token=" .. token .. "; Path=/; HttpOnly; SameSite=None")
+  kong.response.set_header("Set-Cookie", "auth_token=" .. token .. "; Path=/")
   end
 
   return kong.response.exit(302, nil, {

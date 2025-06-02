8116ba04ca07eefc881f81ce9fabf1897a79687e
Jlarrinzal <jose.maria.larrinzal@estudiantat.upc.edu>
2025-06-02 12:27:12 +0200

    fix: Secure

--- kong-plugins/jwt_validator/handler.lua
+++ kong-plugins/jwt_validator/handler.lua
@@ -30,7 +30,7 @@ function plugin:access(conf)
   end
   
   if jwt_obj.verified then
-  kong.response.set_header("Set-Cookie", "auth_token=" .. token .. "; Path=/; HttpOnly; Secure; SameSite=None")
+  kong.response.set_header("Set-Cookie", "auth_token=" .. token .. "; Path=/; HttpOnly; SameSite=None")
   end
 
   return kong.response.exit(302, nil, {
